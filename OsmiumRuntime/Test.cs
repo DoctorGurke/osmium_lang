ca64119858f16da186b8d22e92112b29981f50d2
DoctorGurke <docgurk@gmail.com>
2024-10-24 01:55:22 +0200

    add fibonacci include test to console runner

--- OsmiumRuntime/Test.cs
+++ OsmiumRuntime/Test.cs
@@ -5,16 +5,40 @@ namespace Osmium;
 [TestFixture]
 public class Test
 {
+
+    private ConsoleRunner SetupRunner()
+    {
+        var runtime = new Interpreter.Runtime();
+        return new ConsoleRunner(runtime);
+    }
+
     [Test]
     public void TestInitialization()
     {
-        var runtime = new Interpreter.Runtime();
-        var instance = new ConsoleRunner(runtime);
-
+        // given a runner and basic expression var name and value
+        var runner = SetupRunner();
         string var = "x";
         int val = 5;
-        instance.ProcessInput($"{var}={val}");
-        int value = (int)instance.Members.GetSymbolValue(var);
+
+        // when evaluating the expression
+        runner.ProcessInput($"{var}={val}");
+
+        // then the symbol and value should match
+        int value = (int)runner.Members.GetSymbolValue(var);
         Assert.That(value, Is.EqualTo(val));
+    }
+
+    [Test]
+    public void TestFibonacci()
+    {
+        // given a runner
+        var runner = SetupRunner();
+
+        // when including a script and using a function from it
+        runner.ProcessInput("include fibonacci.script");
+        runner.ProcessInput("x = fibonacci(10);");
+
+        // then the return value should match the expected function
+        Assert.That(runner.Members.GetSymbolValue("x"), Is.EqualTo(55));
     }
 }
