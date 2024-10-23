b76472287e23f41a556396eb539ec0f1e3cebc7f
Doctor Gurke <docgurk@gmail.com>
2024-10-23 17:40:15 +0200

    split OsmiumInterpreter into class library and runtime for test project

--- OsmiumInterpreter/Program.cs
+++ OsmiumInterpreter/Program.cs
@@ -4,41 +4,6 @@ using Osmium.Interpreter;
 
 namespace Osmium;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        {
-            var runtime = new Runtime();
-
-            while (true)
-            {
-                var input = Console.ReadLine();
-                if (input is null)
-                    continue;
-
-                if (input.StartsWith("run "))
-                {
-                    var script = input[4..];
-                    Log.Info($"running {script}");
-                    runtime.RunLocal(Script.Load(script));
-                    continue;
-                }
-                else if (input.StartsWith("include "))
-                {
-                    var script = input[8..];
-                    Log.Info($"including {script}");
-                    runtime.Run(Script.Load(script));
-                    continue;
-                }
-
-                runtime.Run(input);
-            }
-        }
-    }
-
-}
-
 public static class Log
 {
     public static void Space()
