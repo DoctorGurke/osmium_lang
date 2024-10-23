755ae2f99c366dae910bf827d9c70edd04bddf23
DoctorGurke <docgurk@gmail.com>
2024-10-24 01:00:11 +0200

    add test to runtime

---
+++ OsmiumRuntime/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Osmium;
+
+[TestFixture]
+public class Test
+{
+    [Test]
+    public void TestInitialization()
+    {
+        var runtime = new Interpreter.Runtime();
+        var instance = new Runtime(runtime);
+
+        string var = "x";
+        int val = 5;
+        instance.ProcessInput($"{var}={val}");
+        int value = (int)instance.Members.GetSymbolValue(var);
+        Assert.That(value, Is.EqualTo(val));
+    }
+}
