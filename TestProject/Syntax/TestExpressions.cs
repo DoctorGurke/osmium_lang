f65ab4a63fccd9142af047e378679cac0e064891
DoctorGurke <docgurk@gmail.com>
2024-11-03 20:07:12 +0100

    :white_check_mark: Add more Expression syntax tests

--- TestProject/Syntax/TestExpressions.cs
+++ TestProject/Syntax/TestExpressions.cs
@@ -5,12 +5,45 @@ namespace TestProject.Syntax;
 [TestFixture]
 public class TestExpressions
 {
-    private Osmium.Interpreter.Runtime? runtime;
+    private Runtime? runtime;
 
     [SetUp]
     public void SetUp()
     {
-        runtime = new Osmium.Interpreter.Runtime();
+        runtime = new Runtime();
+    }
+
+    [TestCase("x = 5 + (3 + 3) - 4;")]
+    [TestCase("x = 1; y = x * 10 + (1 + x);")]
+    [TestCase("x = [1]; y = x[0] + 10;")]
+    [TestCase("function x: return 1 end; y = x() + 10;")]
+    [TestCase("enum x = [state1]; y = x.state1 + 10;")]
+    public void VerifyExpressions(string input)
+    {
+        Assert.DoesNotThrow(() => { runtime!.Run(input, local: true); });
+    }
+
+    [TestCase("x = 1 * 3;")]
+    [TestCase("x = 10 / 5;")]
+    [TestCase("x = 17 % 2;")]
+    [TestCase("x = 3 + 1;")]
+    [TestCase("x = 4 - 1;")]
+    public void VerifyArithmeticExpression(string input)
+    {
+        Assert.DoesNotThrow(() => { runtime!.Run(input, local: true); });
+    }
+
+    [TestCase("x = * 1;")]
+    [TestCase("x = 1 *;")]
+    [TestCase("x = / 5;")]
+    [TestCase("x = 5 /;")]
+    [TestCase("x = % 1;")]
+    [TestCase("x = 1 %;")]
+    [TestCase("x = 3 +;")]
+    [TestCase("x = 4 -;")]
+    public void VerifyInvalidArithmeticExpression(string input)
+    {
+        Assert.Throws<ParserException>(() => { runtime!.Run(input, local: true); });
     }
 
     [TestCase("x = !true;")]
