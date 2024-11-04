26c4b679e65bee3ebcfdb0143a9d9c375faf8288
DoctorGurke <docgurk@gmail.com>
2024-11-05 00:11:38 +0100

    :white_check_mark: Automatically test scripts/examples .script files

---
+++ OsmiumInterpreter/Tests/TestExamples.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Reflection;
+
+namespace Osmium.Tests;
+
+[TestFixture]
+public class TestExamples
+{
+    public Runtime? Runtime { get; private set; }
+
+    [SetUp]
+    protected void SetUp()
+    {
+        Runtime = null;
+        Runtime = new Runtime();
+    }
+
+    [TestCaseSource(nameof(ExampleScripts))]
+    public void VerifyScript(string script)
+    {
+        Assert.DoesNotThrow(() => Runtime!.Run(Script.Load(script)));
+    }
+
+    public static IEnumerable<string> ExampleScripts()
+    {
+        var executableLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new Exception($"Couldn't find Executable location!");
+        var examplesPath = Path.Combine(executableLocation, "scripts\\examples");
+
+        var scripts = Directory.GetFiles(examplesPath, "*.script", searchOption: SearchOption.AllDirectories);
+        foreach (var script in scripts)
+        {
+            yield return script.Replace($"{executableLocation}\\scripts\\", "");
+        }
+    }
+}
