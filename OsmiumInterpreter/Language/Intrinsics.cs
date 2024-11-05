1c94eb2eb138a75c391d3c3531eecd69ecaaaecb
DoctorGurke <docgurk@gmail.com>
2024-11-05 22:58:15 +0100

    :recycle: use IEnumerable for intrinsics

--- OsmiumInterpreter/Language/Intrinsics.cs
+++ OsmiumInterpreter/Language/Intrinsics.cs
@@ -26,7 +26,7 @@ public static class Intrinsics
         string printString = $"{parameter}";
 
         // list parameter
-        if (parameter is List<object> list)
+        if (parameter is IEnumerable<object> list)
         {
             printString = $"[{string.Join(", ", list.ToArray())}]";
         }
@@ -40,10 +40,10 @@ public static class Intrinsics
         if (args is null || args.Length != 1)
             throw new ArgumentException($"Invalid arg count for function 'length'. Expected: list");
 
-        if (args[0] is not IList<object> list)
+        if (args[0] is not IEnumerable<object> list)
             throw new ArgumentException($"Invalid arg types for function 'length'. Expected: list. Got: {args[0].GetType()}");
 
-        return list.Count;
+        return list.Count();
     }
 
     [IntrinsicFunction(identifier: "foreach", "list", "function")]
@@ -52,7 +52,7 @@ public static class Intrinsics
         if (args is null || args.Length != 2)
             throw new ArgumentException($"Invalid arg count for function 'foreach'. Expected: list, lambda");
 
-        if (args[0] is not IList<object> list || args[1] is not IFunction function)
+        if (args[0] is not IEnumerable<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for function 'foreach'. Expected: list, lambda. Got: {args[0].GetType()}, {args[1].GetType()}");
 
         foreach (var item in list)
@@ -63,12 +63,12 @@ public static class Intrinsics
     }
 
     [IntrinsicFunction(identifier: "map", "list", "function")]
-    public static List<object?> Map(object[] args)
+    public static IEnumerable<object?> Map(object[] args)
     {
         if (args is null || args.Length != 2)
             throw new ArgumentException($"Invalid arg count for function 'map'. Expected: list, lambda");
 
-        if (args[0] is not IList<object> list || args[1] is not IFunction function)
+        if (args[0] is not IEnumerable<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for function 'map'. Expected: list, lambda. Got: {args[0].GetType()}, {args[1].GetType()}");
 
         var returnCollection = new List<object?>();
@@ -89,7 +89,7 @@ public static class Intrinsics
         if (args is null || args.Length != 3)
             throw new ArgumentException($"Invalid arg count for function 'reduce'. Expected: list, lambda, object");
 
-        if (args[0] is not IList<object> list || args[1] is not IFunction function)
+        if (args[0] is not IEnumerable<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for function 'reduce'. Expected: list, lambda, object. Got: {args[0].GetType()}, {args[1].GetType()}, {args[2].GetType()}");
 
         var acc = args[2];
@@ -104,12 +104,12 @@ public static class Intrinsics
     }
 
     [IntrinsicFunction(identifier: "filter", "list", "function")]
-    public static List<object> Filter(object[] args)
+    public static IEnumerable<object> Filter(object[] args)
     {
         if (args is null || args.Length != 2)
             throw new ArgumentException($"Invalid arg count for function 'filter'. Expected: list, lambda");
 
-        if (args[0] is not IList<object> list || args[1] is not IFunction function)
+        if (args[0] is not IEnumerable<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for function 'filter'. Expected: list, lambda. Got: {args[0].GetType()}, {args[1].GetType()}");
 
         var returnCollection = new List<object>();
