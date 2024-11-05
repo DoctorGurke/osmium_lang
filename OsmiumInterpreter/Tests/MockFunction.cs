209dd6e2e45ef69007fa1c0d500f252b56b1ba51
DoctorGurke <docgurk@gmail.com>
2024-11-05 23:24:06 +0100

    :white_check_mark: Add MockFunction to test intrinsics

---
+++ OsmiumInterpreter/Tests/MockFunction.cs
@@ -0,0 +1,12 @@
+﻿using Osmium.Language;
+using Osmium.Language.Types;
+
+namespace Osmium.Tests;
+
+internal sealed class MockFunction : IFunction
+{
+    public object? Invoke(Interpreter visitor, object[] parameters)
+    {
+        throw new NotImplementedException();
+    }
+}
