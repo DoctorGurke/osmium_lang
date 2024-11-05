3afe340196f3174cfd220e823d799c4829b47760
DoctorGurke <docgurk@gmail.com>
2024-11-05 18:35:13 +0100

    :construction: (#5) Add Identifier and Parameters to IntrinsicFunctionAttribute

--- OsmiumInterpreter/Language/Intrinsics.cs
+++ OsmiumInterpreter/Language/Intrinsics.cs
@@ -4,11 +4,20 @@ using System.Text;
 namespace Osmium.Language;
 
 [AttributeUsage(AttributeTargets.Method)]
-public class IntrinsicFunctionAttribute : Attribute { }
+public class IntrinsicFunctionAttribute : Attribute
+{
+    public string Identifier { get; private set; }
+    public string[] Parameters { get; private set; }
+    public IntrinsicFunctionAttribute(string identifier, params string[] parameters)
+    {
+        Identifier = identifier;
+        Parameters = parameters;
+    }
+}
 
 public static class Intrinsics
 {
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "print", "list")]
     public static void Print(object[] args)
     {
         if (args is null || args.Length != 1)
@@ -33,86 +42,86 @@ public static class Intrinsics
         Console.WriteLine($"{printString}");
     }
 
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "length", "list")]
     public static int Length(object[] args)
     {
         if (args is null || args.Length != 1)
             throw new ArgumentException($"Invalid arg count for intrinsic Length. Expected: list");
 
-        if (args[0] is not IList<object> collection)
+        if (args[0] is not IList<object> list)
             throw new ArgumentException($"Invalid arg types for intrinsic Length. Expected: list. Got: {args[0].GetType()}");
 
-        return collection.Count;
+        return list.Count;
     }
 
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "foreach", "list", "function")]
     public static void ForEach(object[] args)
     {
         if (args is null || args.Length != 2)
             throw new ArgumentException($"Invalid arg count for intrinsic ForEach. Expected: list, lambda");
 
-        if (args[0] is not IList<object> collection || args[1] is not IFunction lambda)
+        if (args[0] is not IList<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for intrinsic ForEach. Expected: list, lambda. Got: {args[0].GetType()}, {args[1].GetType()}");
 
-        foreach (var item in collection)
+        foreach (var item in list)
         {
-            lambda.Invoke(new Interpreter(), new object[] { item });
+            function.Invoke(new Interpreter(), new object[] { item });
         }
     }
 
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "map", "list", "function")]
     public static List<object?> Map(object[] args)
     {
         if (args is null || args.Length != 2)
             throw new ArgumentException($"Invalid arg count for intrinsic Map. Expected: list, lambda");
 
-        if (args[0] is not IList<object> collection || args[1] is not IFunction lambda)
+        if (args[0] is not IList<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for intrinsic Map. Expected: list, lambda. Got: {args[0].GetType()}, {args[1].GetType()}");
 
         var returnCollection = new List<object?>();
 
-        foreach (var item in collection)
+        foreach (var item in list)
         {
-            var eval = lambda.Invoke(new Interpreter(), new object[] { item });
+            var eval = function.Invoke(new Interpreter(), new object[] { item });
             returnCollection.Add(eval);
         }
 
         return returnCollection;
     }
 
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "reduce", "list", "function")]
     public static object Reduce(object[] args)
     {
         if (args is null || args.Length != 3)
             throw new ArgumentException($"Invalid arg count for intrinsic Reduce. Expected: list, lambda, object");
 
-        if (args[0] is not IList<object> collection || args[1] is not IFunction lambda)
+        if (args[0] is not IList<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for intrinsic Reduce. Expected: list, lambda, object. Got: {args[0].GetType()}, {args[1].GetType()}, {args[2].GetType()}");
 
         var acc = args[2];
 
-        foreach (var item in collection)
+        foreach (var item in list)
         {
-            acc = lambda.Invoke(new Interpreter(), new object[] { acc, item });
+            acc = function.Invoke(new Interpreter(), new object[] { acc, item });
         }
 
         return acc;
     }
 
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "filter", "list", "function")]
     public static List<object> Filter(object[] args)
     {
         if (args is null || args.Length != 2)
             throw new ArgumentException($"Invalid arg count for intrinsic Filter. Expected: list, lambda");
 
-        if (args[0] is not IList<object> collection || args[1] is not IFunction lambda)
+        if (args[0] is not IList<object> list || args[1] is not IFunction function)
             throw new ArgumentException($"Invalid arg types for intrinsic Filter. Expected: list, lambda. Got: {args[0].GetType()}, {args[1].GetType()}");
 
         var returnCollection = new List<object>();
 
-        foreach (var item in collection)
+        foreach (var item in list)
         {
-            var cond = (bool)(lambda.Invoke(new Interpreter(), new object[] { item }) ?? false);
+            var cond = (bool)(function.Invoke(new Interpreter(), new object[] { item }) ?? false);
             if (cond)
                 returnCollection.Add(item);
         }
@@ -120,16 +129,16 @@ public static class Intrinsics
         return returnCollection;
     }
 
-    [IntrinsicFunction]
+    [IntrinsicFunction(identifier: "assert", "condition", "message?")]
     public static void Assert(object[] args)
     {
         if (args.Length < 1)
             throw new ArgumentException($"Invalid arg count for assert. Expected 2: condition, optional:message. Got {args.Length}.");
 
-        if (args[0] is not bool eval)
+        if (args[0] is not bool condition)
             throw new ArgumentException($"Invalid arg types for assert. Expected condition. Got: {args[0].GetType()}");
 
-        if (eval)
+        if (condition)
             return;
 
         throw new AssertException((args.Length == 2) ? $"{args[1]}" : "");
