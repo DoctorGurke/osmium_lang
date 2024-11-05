6e3944bfb7cb00fee4f2e4215adada06fbbf1769
DoctorGurke <docgurk@gmail.com>
2024-11-05 22:54:58 +0100

    :white_check_mark: Add Print unit tests

---
+++ OsmiumInterpreter/Tests/TestBaseFunctions.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Osmium.Language;
+
+namespace Osmium.Tests;
+
+[TestFixture]
+public class TestBaseFunctions
+{
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [TestCase("Hello, World!")]
+    [TestCase("")]
+    [TestCase(1)]
+    [TestCase(-0.1f)]
+    [TestCase(null)]
+    public void VerifyPrint(params object[] parameters)
+    {
+        Assert.DoesNotThrow(() => Intrinsics.Print(parameters));
+    }
+
+    [TestCase("", "")]
+    [TestCase()]
+    public void VerifyInvalidPrint(params object[] parameters)
+    {
+        Assert.Throws<ArgumentException>(() => Intrinsics.Print(parameters));
+    }
+}
