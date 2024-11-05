620f788c06d520660945c710d04b3f4c8b45b582
DoctorGurke <docgurk@gmail.com>
2024-11-05 18:35:52 +0100

    :construction: (#5) Add Parameters to MethodWrapper

--- OsmiumInterpreter/Language/Types/MethodWrapper.cs
+++ OsmiumInterpreter/Language/Types/MethodWrapper.cs
@@ -6,11 +6,13 @@ public class MethodWrapper : IFunction
 {
     public string Identifier { get; private set; }
     public MethodInfo Method { get; private set; }
+    public string[] Parameters { get; private set; }
 
-    public MethodWrapper(string identifier, MethodInfo method)
+    public MethodWrapper(string identifier, MethodInfo method, string[] parameters)
     {
         Identifier = identifier;
         Method = method;
+        Parameters = parameters;
     }
 
     public object? Invoke(Interpreter visitor, object[] parameters)
