4dfb452317756fbb0d500a97d7997245eb587795
DoctorGurke <docgurk@gmail.com>
2024-11-05 18:22:15 +0100

    :construction: (#5) Add MethodWrapper to invoke Intrinsic C# MethodInfo as IFunction

---
+++ OsmiumInterpreter/Language/Types/MethodWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Osmium.Language.Types;
+
+public class MethodWrapper : IFunction
+{
+    public string Identifier { get; private set; }
+    public MethodInfo Method { get; private set; }
+
+    public MethodWrapper(string identifier, MethodInfo method)
+    {
+        Identifier = identifier;
+        Method = method;
+    }
+
+    public object? Invoke(Interpreter visitor, object[] parameters)
+    {
+        throw new NotImplementedException();
+    }
+}
