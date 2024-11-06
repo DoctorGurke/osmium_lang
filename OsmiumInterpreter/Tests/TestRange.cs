48ef5d7640049dd02ff92735873cd40a4f828b67
DoctorGurke <docgurk@gmail.com>
2024-11-06 01:48:49 +0100

    :white_check_mark: Add Range tests

---
+++ OsmiumInterpreter/Tests/TestRange.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Osmium.Tests;
+
+[TestFixture]
+public class TestRange
+{
+    [SetUp]
+    public void SetUp()
+    {
+
+    }
+
+    [TestCase(null, 1)]
+    [TestCase(1, null)]
+    [TestCase(0, 1)]
+    public void VerifyConstructor(int? start, int? end)
+    {
+        Assert.DoesNotThrow(() => { new Language.Types.Range(start, end); });
+    }
+
+    [TestCase(null, null)]
+    [TestCase(3, 1)]
+    public void VerifyInvalidConstructor(int? start, int? end)
+    {
+        Assert.Throws<ArgumentException>(() => { new Language.Types.Range(start, end); });
+    }
+}
