feee0cf07c48199223f2d9bc04c7d185b0eaadb5
DoctorGurke <docgurk@gmail.com>
2024-11-06 01:39:45 +0100

    :recycle: delete Log.Space

--- OsmiumInterpreter/Log.cs
+++ OsmiumInterpreter/Log.cs
@@ -14,14 +14,6 @@ public static class Log
     /// <summary>
     /// Used for debug logging. Silenced by default.
     /// </summary>
-    public static void Space()
-    {
-        Info(">");
-    }
-
-    /// <summary>
-    /// Used for debug logging. Silenced by default.
-    /// </summary>
     /// <param name="content"></param>
     public static void Info(object content)
     {
